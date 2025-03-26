bf9d6e8f49f19fd6e4c78d9cd70c6132b95b3b29
Yann CHIQUELLO <ychiquello@hotmail.com>
2025-03-26 17:03:28 +0100

    pour la dev de vercel 2

--- server2.js
+++ server2.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route pour les pages HTML
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '.', 'index.html'));
 });
 
 app.get('/actions', (req, res) => {
